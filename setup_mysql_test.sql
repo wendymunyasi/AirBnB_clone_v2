da35797d72d9b51441c8285c520b75cb7146d3e8
wendy munyasi <wendymunyasi@gmail.com>
2022-09-13 23:41:07 +0300

    Change hbnb_dev to hbnb_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,5 +6,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 -- hbnb_test should have all privileges on the database hbnb_test_db only.
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
--- hbnb_dev should have SELECT privilege on the database performance_schema.
+-- hbnb_test should have SELECT privilege on the database performance_schema.
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
