b81945c52b1b8e338039fe5de8d6bc018d87e606
wendy munyasi <wendymunyasi@gmail.com>
2022-10-01 11:06:52 +0300

    Deploy web static to web servers - task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,74 +28,44 @@ def do_deploy(archive_path):
         # Uncompress the archive to the folder,
         # /data/web_static/releases/<archive filename without extension>
         # on the web server
-        wExtension = archive_path.replace('/', ' ')
-        wExtension = shlex.split(wExtension)
-        wExtension = wExtension[-1]
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-        # Get name without extension
-        noExtention = wExtension.replace('.', ' ')
-        noExtention = shlex.split(noExtention)
-        noExtention = noExtention[0]
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
 
-        releases_path = "/data/web_static/releases/{}/".format(noExtention)
-        if releases_path.failed:
-            print("failed to create archive directory for release...")
-            return False
-
-        tmp_path = "/tmp/{}".format(wExtension)
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
 
         # upload the archive to the /tmp/ directory of the web server
-        uploaded = put(archive_path, "/tmp/")
-        if uploaded.failed:
-            return False
+        put(archive_path, "/tmp/")
 
         # Create new directory for release
-        new_dir = run("mkdir -p {}".format(releases_path))
-        if new_dir.failed:
-            print("failed to create archive directory for relase...")
-            return False
+        run("mkdir -p {}".format(releases_path))
 
         # Untar archive
-        untar = run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        if untar.failed:
-            print("failed to untar archive...")
-            return False
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
 
         # Delete the archive from the web server
-        deleted = run("rm {}".format(tmp_path))
-        if deleted.failed:
-            print("failed to remove archive...")
-            return False
+        run("rm {}".format(tmp_path))
 
         # Move extraction to proper directory
-        mov = run("mv {}web_static/* {}".format(releases_path, releases_path))
-        if mov.failed:
-            print("failed to move extraction to proper directory...")
-            return False
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
 
         # Delete first copy of extraction after move
-        rem_fir = run("rm -rf {}web_static".format(releases_path))
-        if rem_fir.failed:
-            print("failed to remove first copy of extraction after move...")
-            return False
+        run("rm -rf {}web_static".format(releases_path))
 
         # Delete the symbolic link /data/web_static/current from the web server
-        sym_old = run("rm -rf /data/web_static/current")
-        if sym_old.failed:
-            print("failed to clean up old release...")
-            return False
+        run("rm -rf /data/web_static/current")
 
         # Create new the symbolic link /data/web_static/current on web server,
         # linked to the new version of your code,
         # (/data/web_static/releases/<archive filename without extension>
-        symNew = run("ln -s {} /data/web_static/current".format(releases_path))
-        if symNew.failed:
-            print("failed to create link to new release...")
-            return False
-
+        run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-
     except Exception:
         print("Could not deploy")
         return False
