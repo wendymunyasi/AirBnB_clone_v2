5c7f82aa1d955dd698193bcb0729b93dd4fa6c23
wendy munyasi <wendymunyasi@gmail.com>
2022-10-01 11:28:06 +0300

    Feature: Update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -31,7 +31,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
 # This should be recursive; everything inside should be created/owned by this user/group.
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 # (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
