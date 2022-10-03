0a1e2946f2394871cb08559d0c4ba0f6977619fb
wendy munyasi <wendymunyasi@gmail.com>
2022-10-03 12:38:07 +0300

    Add do_pack() function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,12 +5,37 @@ script that distributes archive to webservers
 import os.path
 from fabric.api import *
 from fabric.operations import run, put
-import shlex
+from datetime import datetime
 
 
 env.hosts = ['3.227.217.150', '3.95.27.202']
 env.user = "ubuntu"
 
+def do_pack():
+    """ generates a .tgz archive from the contents of the web_static
+
+    All files in the folder web_static must be added to the final archive.
+    All archives must be stored in the folder versions.
+    The name of the archive created must be:
+        web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive path if the archive has
+    been correctly generated. Otherwise, it should return None.
+
+    Returns:
+        fabric.operations._AttributeString: archive path.
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # create folder versions if it doesn’t exist
+    local("mkdir -p versions")
+
+    # extract the contents of a tar archive
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(now))
+    if result.failed:
+        return None
+    else:
+        return result
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers.
