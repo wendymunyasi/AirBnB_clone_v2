a1438faa22b9aacba84494e6aa6b83e690fcf42d
wendy munyasi <wendymunyasi@gmail.com>
2022-10-17 13:55:18 +0300

    Add script that starts a Flask web app - task 5

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Initializes application"""
+
+
+# import Flask class from flask module
+from flask import Flask
+
+# create an instance called app of the class by passong the __name__ variable
+app = Flask(__name__)
+
+
