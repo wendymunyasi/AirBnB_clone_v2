ed26a57a23eb51897e4b9eeb0cdcc74b567fb251
wendy munyasi <wendymunyasi@gmail.com>
2022-10-17 11:22:49 +0300

    Add script that starts a Flask web app - task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"script that starts a Flask web application"
+# import Flask class from flask module
+from flask import Flask
+
+# create an instance called app of the class by passong the __name__ variable
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """serves the home page
+
+    Returns:
+        str: text on the index page
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "_main__":
+    app.run(debug=True)
