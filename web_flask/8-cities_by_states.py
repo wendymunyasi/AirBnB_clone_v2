c4b246108ba15dca2457f8eb58c0612092aede2b
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 12:07:22 +0300

    Attempt to fix codefails - task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -18,12 +18,12 @@ app.url_map.strict_slashes = False
 def teardown_db(exception=None):
     """removes the current SQLAlchemy Session
     """
-    if storage is not None:
-        storage.close()
+    # if storage is not None:
+    storage.close()
 
 
 @app.route('/cities_by_states')
-def states_list(n=None):
+def cities_list(n=None):
     """displays a HTML page: inside the tag BODY"""
     states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
